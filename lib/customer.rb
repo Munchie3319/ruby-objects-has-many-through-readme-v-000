50c0505d88a6c72a92f7e97ed58e3d91a3110844
rosa lee <rosalee0116@gmail.com>
2020-04-14 21:20:57 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,41 +1,34 @@
 class Customer
-  attr_accessor :name, :age
 
-  @@all = [ ]
+ 
+  end 		   @@all = []
 
-  def initialize(name, age)
-    @name = name
-    @age = age
-    @@all << self
-  end
+    attr_accessor :name, :age
 
-  def self.all
-    @@all
-  end
+    def initialize(name, age)
+     @name = name
+     @age = age
+     @@all << self
+   end
 
-  def new_meal(waiter, total, tip=0)
-    Meal.new(waiter, self, total, tip)
-  end
+    def self.all
+     @@all
+   end
 
-  def meals
-    Meal.all.select do |meal|
-      meal.customer == self.end
-    end
+    def new_meal(waiter, total, tip=0)
+     Meal.new(waiter, self, total, tip)
+   end
+
+    def meals
+     Meal.all.select do |meal|
+       meal.customer == self
+     end
+   end
 
     def waiters
-      meals.map do |meal|
-        meal.waiter
-      end
+     meals.map do |meal|
+       meal.waiter
+     end
+   end
 
-      terrance = Customer.new("Terrance", 27)
-jason = Waiter.new("Jason", 4)
-andrew = Waiter.new("Andrew", 7)
-yomi = Waiter.new("Yomi", 10)
-
-terrance.new_meal(jason, 50, 6)
-terrance.new_meal(andrew, 60, 8)
-terrance.new_meal(yomi, 30, 4)
-
-terrance.waiters  
-terrance.waiters.length
-end
+  end
