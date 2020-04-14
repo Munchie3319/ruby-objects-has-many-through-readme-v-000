7639b1c1f7d4c6f1a6f582e088318373b28a033c
rosa lee <rosalee0116@gmail.com>
2020-04-14 20:47:21 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -17,8 +17,18 @@ class Customer
     Meal.new(waiter, self, total, tip)
   end
 
-  def waiters
-    meals.map do |meal|
-      meal.waiter
-    end
+  terrance = Customer.new("Terrance", 27)
+  jason = Waiter.new("Jason", 4)
+  andrew = Waiter.new("Andrew", 7)
+  yomi = Waiter.new("Yomi", 10)
+ 
+  terrance.new_meal(jason, 50, 6)
+  terrance.new_meal(andrew, 60, 8)
+  terrance.new_meal(yomi, 30, 4)
 end
+
+
+
+
+
+
