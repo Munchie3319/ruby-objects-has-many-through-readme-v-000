0e53763ad87d53e93d435d735e5d861bd7dcec58
rosa lee <rosalee0116@gmail.com>
2020-04-14 20:31:48 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,28 @@
 class Customer
-  
+  attr_accessor :name, :age
+
+  @@all = [ ]
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_meal(waiter, total, tip=0)
+    Meal.new(waiter, self, total, tip)
+  end
+
+  sam = Customer.new("Sam", 27)
+  pat = Waiter.new("Pat", 2)
+  alex = Waiter.new("Alex", 5)
+
+  sam.new_meal(pat, 50, 10)
+  sam.new_meal(alex, 20, 3)
+  pat.new_meal(sam, 30, 5)
+end
 end
